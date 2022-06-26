b31aba157af4c2a271c00cb88ee8713716aae55e
relkimm <52807468+relkimm@users.noreply.github.com>
2022-06-26 16:24:20 +0900

    refactor: 온도의_최대값 초기 계산 리팩토링

--- src/온도의_최대값/index.js
+++ src/온도의_최대값/index.js
@@ -3,10 +3,13 @@ const digree = [3, -2, -4, -9, 0, 3, 7, 13, 8, -3];
 const n = 10;
 const k = 2;
 
-let max = digree[0] + digree[1];
-let sum = digree[0] + digree[1];
+let sum = 0;
+for (let i = 0; i < k; i++) {
+  sum += digree[i];
+}
+let max = sum;
 
-for (let i = 2; i <= n - k; i++) {
+for (let i = k; i <= n - k; i++) {
   sum = sum + digree[i] - digree[i - k];
   if (sum > max) {
     max = sum;
