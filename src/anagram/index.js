597f88b290231769be67a5578bcaeecb657db5e8
relkimm <52807468+relkimm@users.noreply.github.com>
2022-06-26 21:01:29 +0900

    refactor: anagram 중복 로직 리팩토링

--- src/anagram/index.js
+++ src/anagram/index.js
@@ -4,46 +4,31 @@ function solution(a, b) {
   if (typeof a !== "string" || typeof b !== "string") {
     return false;
   }
-
-  const x = [];
-  const y = [];
-  for (const char of a) {
-    const charCode = char.charCodeAt();
-    if (charCode >= 65 && charCode <= 90) {
-      const upperIndex = charCode - 65;
-      if (x[upperIndex]) {
-        x[upperIndex]++;
+  function parse(a) {
+    const array = [];
+    for (const char of a) {
+      const charCode = char.charCodeAt();
+      if (charCode >= 65 && charCode <= 90) {
+        const upperIndex = charCode - 65;
+        if (array[upperIndex]) {
+          array[upperIndex]++;
+        } else {
+          array[upperIndex] = 1;
+        }
       } else {
-        x[upperIndex] = 1;
-      }
-    } else {
-      const lowerIndex = charCode - 71;
-      if (x[lowerIndex]) {
-        x[lowerIndex]++;
-      } else {
-        x[lowerIndex] = 1;
-      }
-    }
-  }
-
-  for (const char of b) {
-    const charCode = char.charCodeAt();
-    if (charCode >= 65 && charCode <= 90) {
-      const upperIndex = charCode - 65;
-      if (y[upperIndex]) {
-        y[upperIndex]++;
-      } else {
-        y[upperIndex] = 1;
-      }
-    } else {
-      const lowerIndex = charCode - 71;
-      if (y[lowerIndex]) {
-        y[lowerIndex]++;
-      } else {
-        y[lowerIndex] = 1;
+        const lowerIndex = charCode - 71;
+        if (array[lowerIndex]) {
+          array[lowerIndex]++;
+        } else {
+          array[lowerIndex] = 1;
+        }
       }
     }
+    return array;
   }
+
+  const x = parse(a);
+  const y = parse(b);
 
   for (const i of range(52)) {
     if (x[i] !== y[i]) {
